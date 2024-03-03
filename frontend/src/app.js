d01e153f8924904a387ca11a2b4bd8b5a5778a6c
alexanderjalexander <alexj18082002@gmail.com>
2024-03-02 20:43:35 -0500

    added new routes

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
-import Home from "./home"
-import Login from "./login"
+import Home from "./pages/home"
+import Login from "./pages/login"
+import Registration from "./pages/registration"
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Registration />} />
                 </Routes>
             </BrowserRouter>
         </div>

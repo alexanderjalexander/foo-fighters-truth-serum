dae21452ee4559f21041772925e598ef35a82907
alexanderjalexander <alexj18082002@gmail.com>
2024-03-16 00:55:48 -0400

    checkPassword Test Cases

--- frontend/src/test/validation.test.js
+++ frontend/src/test/validation.test.js
@@ -26,3 +26,17 @@ test('checkEmail Fail 2', () => {
     expect(validation.checkEmail('Nintendo CEO @ Tony Hawk . com')).toBe(string);
     expect(validation.checkEmail('&&&&&&&&@&&&&&&&&.&&&&&&&&')).toBe(string);
 });
+
+test('checkPassword Valid', () => {
+    expect(validation.checkPassword('Stevens42!')).toBe("");
+    expect(validation.checkPassword('Dingus2592@@@')).toBe("");
+    expect(validation.checkPassword('notThePresident125255@*')).toBe("");
+});
+
+test('checkPassword Fail', () => {
+    expect(validation.checkPassword('a')).toBe('Password must be at least 10 characters long.');
+    expect(validation.checkPassword('AAAAAAAAAAAAAAAAAAAA')).toBe("Password must have at least 1 lower case letter.");
+    expect(validation.checkPassword('aaaaaaaaaaaaaaaaaaaa')).toBe("Password must have at least 1 upper case letter.");
+    expect(validation.checkPassword('AaAaAaAAAAAAAAAAAAAA')).toBe("Password must have at least 1 number.");
+    expect(validation.checkPassword('AaAaAaAa231231232333')).toBe("Password must have at least 1 non alphanumeric character.");
+});
